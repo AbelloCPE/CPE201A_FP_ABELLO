2e1fe0c73b6511f3b12ce186f594e46f606a0ab6
AbelloCPE <qrnabello@tip.edu.ph>
2025-11-11 17:18:18 +0800

    Delete all files and reset repository

--- addFood.h
+++
@@ -1,8 +0,0 @@
-#ifndef ADD_FOOD_H
-#define ADD_FOOD_H
-#include <string>
-using namespace std;
-
-void addFoodToList(const string& username);
-
-#endif
--- viewReport.h
+++
@@ -1,8 +0,0 @@
-#ifndef VIEW_REPORT_H
-#define VIEW_REPORT_H
-#include <string>
-using namespace std;
-
-void viewReport(const string& username, double bmr);
-
-#endif
